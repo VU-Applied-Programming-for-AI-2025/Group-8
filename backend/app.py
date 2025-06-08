a5be3d9d4c1cf1192fe12a75adb742f2addf0134
KEiK0o <dora.khoshimova@gmail.com>
2025-06-08 20:56:48 +0200

    Dora-homepage and submission function- so the user can enter their symptoms
    finished function for displaying the homepage and handling the submission from the user, this function will then redirect to the next function to analyze the results

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,37 @@
 ## develop your flask app here ##
+from flask import Flask, render_template, url_for, request, redirect
 import os
-
 from groq import Groq
 
+app = Flask(__name__)
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Explain the importance of fast language models",
-        }
-    ],
-    model="llama-3.3-70b-versatile",
-)
+#homepage
+@app.route("/home", methods = ["GET", "POST"])
+def home_page():
+    """
+    This function displays the homepage and handles the submission from the user for their symptoms. After symptoms are provided, redirects to /results
+    with the symptoms being the url parameter. 
+    """
+    if request.method == "POST":
+        symptoms = request.form.get("symptoms")
+        return redirect(url_for("analyze_symptoms", symptoms = symptoms))
+    return render_template("index.html")
 
-print(chat_completion.choices[0].message.content)
+#analyze symptoms
+
+    
+
+# chat_completion = client.chat.completions.create(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "Explain the importance of fast language models",
+#         }
+#     ],
+#     model="llama-3.3-70b-versatile",
+# )
+
+# print(chat_completion.choices[0].message.content)
