05ff3cee8bafc3a6be09d735d57b0d9195c40d34
sevvalyelmer <sevvalbaharyelmer@gmail.com>
2025-06-09 12:28:12 +0200

    sevval/removed redundant data

--- backend/app.py
+++ backend/app.py
@@ -11,61 +11,6 @@ app = Flask(
     __name__,
     template_folder="../frontend/templates", 
 )
-
-"""UserList: Dict[int, User] = {} # key = user id, value = user object
-UserList[1] = User(1, 21, "Sevval")
-UserList[2] = User(2, 44, "Feyza")
-UserList[3] = User(3, 34, "Omer")
-UserList[4] = User(4, 88, "Mustafa")
-RecipeList: Dict[int, Recipe] = {}
-RecipeList[101] = Recipe(101, "Sarma")
-RecipeList[102] = Recipe(102, "Kebap")
-RecipeList[103] = Recipe(103, "Corba")"""
-
-#SavingList: Dict[int, List[int]] = {} # key = user id, value = list of recipe id's
-
-"""@app.route('/savings/<user_idx>/<recipe_idx>', methods = ['POST'])
-def add(user_idx, recipe_idx):
-    user_id = int(user_idx)
-    recipe_id = int(recipe_idx)
-    if user_id not in SavingList:
-        SavingList[user_id] = []
-        
-    if recipe_id in SavingList[user_id]:
-        return "already exist"
-    
-    SavingList[user_id].append(recipe_id)
-    return "OK"
-
-@app.route('/savings/<user_idx>/<recipe_idx>', methods = ['DELETE'])
-def remove(user_idx, recipe_idx):
-    user_id = int(user_idx)
-    recipe_id = int(recipe_idx)
-    if user_id not in SavingList:
-        SavingList[user_id] = []
-    
-    if recipe_id not in SavingList[user_id]:
-        return "not exist"
-    
-    if recipe_id in SavingList[user_id]:
-        SavingList[user_id].remove(recipe_id)
-    
-    return "OK" 
-
-@app.route('/savings/<user_idx>', methods = ['GET'])
-def recipe_list(user_idx):
-    user_id = int(user_idx)
-    if user_id not in SavingList:
-        SavingList[user_id] = []
-    
-    SavedRecipes = []
-    for recipe_id in SavingList[user_id]:
-        recipe = RecipeList[recipe_id]
-        SavedRecipes.append(recipe.toJSON())
-
-    return jsonify(SavedRecipes)"""
-
-
 app.secret_key = "VerySupersecretKey"  # A secret key for the sessions.
 spoonacular_api_key = os.getenv("API_KEY") # create an account in spoonacular.com, get api key, put in .env, and run "pip install python-dotenv"
 
