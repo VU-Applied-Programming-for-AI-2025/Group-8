4e08817a09b441cf7894ad1c7c4ee3762cfc8cb0
KEiK0o <dora.khoshimova@gmail.com>
2025-06-09 15:18:41 +0200

    Dora-analyze function and homepage - function for analyzing symptoms with the llm and homepag.html for inputting symptoms
    function for analyzing the symptoms is done with the basics, the function sends the prompt to the groq llm and then displays it to the results.html (not done yet). the homepage with a simple layout for now to see if it works  ok

--- backend/app.py
+++ backend/app.py
@@ -18,20 +18,34 @@ def home_page():
     if request.method == "POST":
         symptoms = request.form.get("symptoms")
         return redirect(url_for("analyze_symptoms", symptoms = symptoms))
-    return render_template("index.html")
+    return render_template("homepage.html")
 
 #analyze symptoms
+@app.route("/results")
+def analyze_symptoms():
+    symptoms = request.args.get("symptoms")
 
+    ai_prompt = f"analyze the possible vitamins/nutrients the user might be lacking for hair loss and acne. give the results in a structured way like this:\n- vitamin/nutrient that might be lacking\n- why this vitamin/nutrient matters\n- foods to eat to fix this issue\n"
     
+    ## llm should incorporate the pesonal details of the user like allergies, pregnancy, etc 
+    
+    analysis_results = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": ai_prompt,
+            }
+        ],
+        temperature=1,
+        max_completion_tokens=1024,
+        top_p=1,
+        stream=True,
+        stop=None
+    )
+    analysis = analysis_results.choices[0].message.content
+    
+    return render_template("results.html", symptoms = symptoms, analysis = analysis)
 
-# chat_completion = client.chat.completions.create(
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "Explain the importance of fast language models",
-#         }
-#     ],
-#     model="llama-3.3-70b-versatile",
-# )
-
-# print(chat_completion.choices[0].message.content)
+if __name__ == "__main__":
+    app.run(debug=True)
