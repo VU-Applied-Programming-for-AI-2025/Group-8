c6f9d4f61d4157cefb1f817349e8847d44ff7dbb
ferufera <97635255+ferufera@users.noreply.github.com>
2025-06-21 23:36:07 +0200

    responsive menu implementation

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -22,3 +22,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myNavLeft");
+    if (x.className === "nav-left") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-left";
+    }
+}
